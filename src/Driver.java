11edf27f4d3c790993e5aabf6f35409141d94046
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-03-24 04:12:24 -0400

    Add files via upload

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,6 +12,9 @@ public class Driver {
 	static HashMap<String,String> map=new HashMap<String,String>(); 
     static int number;
 	
+    
+    
+    
   public static void main(String[] args)
   {
 	  System.out.println("Welcome to Blibibliography Factory !!!\n");
@@ -27,27 +31,17 @@ public class Driver {
 	 String path="C:\\Users\\lenovo\\Desktop\\Files-249\\Latex"; 
 	 intializemap();
 	 
-	 for (int i=1; i<=1; ++i)
+	 for (int i=1; i<=10; ++i)
 	 {
 	    number=0;
-	    Scanner File=  existfile(path+i);
+	    Scanner File= existfile(path+i);
 	    
-	    if (!processFileForValidation(File))
-	    {
-	     System.out.println("The File Latex"+i+" is Invalid");	
-	     System.exit(0);
-	    }
-	    
-	    PrintWriter iee=createfiles(path,"IEEE"+i+".json");
-	    PrintWriter acm=createfiles(path,"ACM"+i+".json");
-	    PrintWriter nj=createfiles(path,"NJ"+i+".json");
-
-
+	    processFileForValidation(File,i,path);
+         
+	    File.close();
 	 }		 
 	  
   }
-  
-  
   
   
   
@@ -59,16 +53,13 @@ public class Driver {
 	   
 	   try
 	   {
-	    file=new Scanner(new FileInputStream(path));
-	    
-	    if (file==null)
-	     throw new FileNotFoundException();  	
+	    file=new Scanner(new FileInputStream(path)); 	
 	   }
 	   
       catch(FileNotFoundException e)
 	   {
     	  System.out.println("\nCould not open input file "+path.substring(34)+" for reading. Please check if file exists! Program will terminate after closing any opened files");
-          System.exit(0);
+      	  System.exit(0);
 	   }
 	  
 	   return file;
@@ -81,8 +72,7 @@ public class Driver {
 	   
 	   try
 	   {
-	    out=new PrintWriter(new FileOutputStream(path.substring(0,34)+"\\Files\\"+name));    
-	     //throw new FileNotFoundException();  	
+	    out=new PrintWriter(new FileOutputStream(path.substring(0,34)+"Files\\"+name,true));      	
 	   }
 	   
       catch(FileNotFoundException e)
@@ -91,34 +81,20 @@ public class Driver {
           System.exit(0);
 	   }
 	  
-	   return out; 
-	   
-	   
+	   return out;   
    }
    
    
-   public static boolean processFileForValidation(Scanner file)
+   public static void processFileForValidation(Scanner file,int index,String path)
    {   
-	  String info=convertintostring(file);
-              	
-        /* 
-         if (!s.equals(""))
-         {
-        	
-        	if (s.indexOf("=")==-1)
-        	 continue;
-        	
-          info=s.substring(s.indexOf("=")+1).trim();
-          System.out.println(info);
-         } 
-         
-          //if (info.equals(""))
-           //return false;	 
-          */
-        // System.out.println(info);
-         
+	     String info=convertintostring(file);
+   
          String[] parts=converttoparts(info);
 	    
+        PrintWriter ieee=createfiles(path,"IEEE"+index+".json");
+  	    PrintWriter acm=createfiles(path,"ACM"+index+".json");
+  	    PrintWriter nj=createfiles(path,"NJ"+index+".json");
+         
          
          for (int i=0; i<parts.length; ++i)
          {
@@ -132,20 +108,79 @@ public class Driver {
            map.put("doi", parts[i].substring(parts[i].indexOf("doi=")+4,parts[i].indexOf("~", parts[i].indexOf("doi=")))  ); 
            map.put("month", parts[i].substring(parts[i].indexOf("month=")+6,parts[i].indexOf("~", parts[i].indexOf("month=")))  ); 
            
+           boolean valid=true;
            
-           for (Map.Entry<String, String> entry : map.entrySet()) {
-        	
-        	 if (entry.getValue().equals(""))
-              return false;   
-        	   
-   		    System.out.println(entry.getKey() + " = " + entry.getValue());
-           }
+           for (Map.Entry<String, String> entry : map.entrySet()) 
+           {
+        	 try
+        	 {
+        	  if (entry.getValue().equals(""))
+        	   throw new FileInvalidException("The File Latex"+index+" is Invalid"+"\t Field "+entry.getKey()+" is empty");         	
+        	 } 
+        	 
+        	 
+        	 catch (FileInvalidException e)
+        	 {
+        		 valid=false; 
+        		 System.out.println(e.getMessage());
+        		 
+        		 ieee.close();
+        		 acm.close();
+        		 nj.close();
+        		 
+        		 
+        		 new File(path.substring(0,34)+"Files\\"+"IEEE"+index+".json").delete();
+        		 new File(path.substring(0,34)+"Files\\"+"ACM"+index+".json").delete();
+        		 new File(path.substring(0,34)+"Files\\"+"NJ"+index+".json").delete();
+        		 
+        		 break;
+        	 }      	 
+        	         	 
+           } 
            
-           System.out.println();
-         }	 
-	  
-	  return true;
+           if (valid)
+           {
+    	    writeieee(ieee);
+    	    writeacm(acm);
+    	    writenj(nj);
+           }  
+         }
+         
+         
+  	      ieee.close();
+		  acm.close();
+		  nj.close();
+
    }
+   
+   
+   
+   
+   public static void writeieee(PrintWriter ieee)
+   {
+	   ieee.print(map.get("author").replace("and", ",")+". ");
+	   ieee.print(" \""+map.get("title") + "\", " + map.get("journal")+ ", vol." + map.get("volume") + ", no." + map.get("number")+ ", p."+map.get("pages"));
+	   ieee.println(", "+map.get("month")+" "+map.get("year")+"."+"\n");
+	   
+	  // System.out.println("\n");
+   }
+   
+   public static void writeacm(PrintWriter acm)
+   {
+	   acm.print(map.get("author").substring(0,map.get("author").indexOf("and")) + "et all. " );
+	   acm.print(map.get("year")+". "+ map.get("title") + ". " + map.get("journal")+ ". " + map.get("volume") +", "+ map.get("number")+"("+map.get("year")+")" );
+	   acm.println(", "+map.get("pages")+". "+"DOI: https://doi.org/"+map.get("doi")+"."+"\n");
+   }
+   
+   
+   public static void writenj(PrintWriter nj)
+   {
+	   nj.print(map.get("author").replace("and", "&")+". ");
+	   nj.print(map.get("title") + ". " + map.get("journal")+ ". " + map.get("volume") + ", "+map.get("pages"));
+       nj.println("("+map.get("year")+")" +"."+ "\n");
+   }
+   
+   
    
    
    
@@ -228,8 +263,8 @@ public class Driver {
      	 ++index;	 
        }	
     	
-        for (int i=0; i<parts.length; ++i)
-    	System.out.println(parts[i]);
+        //for (int i=0; i<parts.length; ++i)
+    	//System.out.println(parts[i]);
        
     	return parts;
     }
